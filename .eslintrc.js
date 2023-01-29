f9a6722a251e2dcfb4aa461718d7cb44fc9678c5
Mikalai Biadrytski <nickbiadrytski@gmail.com>
2023-01-29 21:39:01 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-console': 'off',
+  },
+};
